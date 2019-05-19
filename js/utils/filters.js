7a08e6023a05cc589f96bb07b6821d20e3185c43
anadurand <ana.dc1004@gmail.com>
2019-05-19 01:42:18 -0500

    changes

--- js/utils/filters.js
+++ js/utils/filters.js
@@ -2,6 +2,7 @@
 
 const filterByDistrict = (stations,query) => {
   return stations.filter( (station) => {
+    console.log(station);
     return station.producto.toLowerCase().indexOf(query.toLowerCase()) > -1;
   });
 }
